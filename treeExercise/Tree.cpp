f99054cd7c89fdc749ea42f3cb6a8a631d3ddb81
arg6549 <arg6549@rit.edu>
2016-10-05 21:02:31 -0400

    Pseudocode written
    Getting the logic started

--- treeExercise/Tree.cpp
+++ treeExercise/Tree.cpp
@@ -6,7 +6,57 @@ Tree::Tree()
 {
 }
 
+Tree::Tree(int lvls)
+{
+	levels = lvls;
+	//int* tree[2^levels - 1];
+	//tree[0] = 1
+
+	//while !done
+		//if leftneighbor(index) == -1
+			//left child (2*index + 1) = 1
+		//else
+			//left child(2*index + 1) = leftneighbor + tree[index]
+
+
+		//if rightneighbor(index) == -1
+			//right child (2*index + 2) = 1
+		//else
+			//right child(2*index + 2) = rightneighbor + tree[index]
+		
+		//index+=2
+
+	//if(index = 2^levels-1)
+	//done
+
+	//printTree
+}
+
+void Tree::printTree()
+{
+}
+
 
 Tree::~Tree()
 {
+}
+
+int Tree::parent(int index)
+{
+	//parent is math.floor((index-1)/2)
+	return 0;
+}
+
+int Tree::leftNeighbor(int index)
+{
+	//if parent's parent exists, return parent's parent's left child
+	//if doesnt exist return -1
+	return 0;
+}
+
+int Tree::rightNeighbor(int index)
+{
+	//if parent's parent exists, return parent's parent's left child
+	//if doesnt exist return -1
+	return 0;
 }
--- treeExercise/Tree.h
+++ treeExercise/Tree.h
@@ -3,6 +3,14 @@ class Tree
 {
 public:
 	Tree();
+	Tree(int lvls);
+	void printTree();
 	~Tree();
+private:
+	int levels;
+	int parent(int index);
+	int leftNeighbor(int index);
+	int rightNeighbor(int index);
+	int index = 0;
 };
 
